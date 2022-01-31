1f29ff2ed69ab17714f7bdbf6e3617b5e046a762
Vida Maleki <vida.basmani@gmail.com>
2022-01-31 00:05:57 -0800

    file modifed in helper function lesson

--- precourse/game.py
+++ precourse/game.py
@@ -5,23 +5,28 @@ RANGE_HIGH = 100
 
 random_number = random.randint(RANGE_LOW, RANGE_HIGH)
 
-def guss_the_number():
-    user_input_string = input("Guess the number: ")
+def guess_the_number():
+    user_input = get_number_from_user()
+    
+    if user_input < RANGE_LOW or user_input > RANGE_HIGH:
+        print(f"Your guess is out of bounds.")
+        print(f"It must be between {RANGE_LOW} and {RANGE_HIGH}")
+    elif user_input == random_number:
+        print("You guessed the number!  Good job!")
+    elif user_input > random_number:
+        print("Your guess is too high")
+    elif user_input < random_number:
+        print("Your guess is too low")
+    
 
+def get_number_from_user():
+    user_input_string = input("Guess the number: ")
+    user_input = None
     if user_input_string.isnumeric():
         user_input = int(user_input_string)
-    
-        if user_input < RANGE_LOW or user_input > RANGE_HIGH:
-            print(f"Your guess is out of bounds.")
-            print(f"It must be between {RANGE_LOW} and {RANGE_HIGH}")
-        elif user_input == random_number:
-            print("You guessed the number!  Good job!")
-        elif user_input > random_number:
-            print("Your guess is too high")
-        elif user_input < random_number:
-            print("Your guess is too low")
-    
     else:
         print("You must input a number!")
-    
-guss_the_number()
+        
+    return user_input
+
+guess_the_number()
