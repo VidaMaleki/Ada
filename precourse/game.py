e1f16b6492cd73e6b87dbb0328c0e62c849ae66a
Vida Maleki <vida.basmani@gmail.com>
2022-02-09 17:05:00 -0800

    file modified in function lesson

--- precourse/game.py
+++ precourse/game.py
@@ -6,27 +6,35 @@ RANGE_HIGH = 100
 random_number = random.randint(RANGE_LOW, RANGE_HIGH)
 
 def guess_the_number():
-    user_input = get_number_from_user()
-    
-    if user_input < RANGE_LOW or user_input > RANGE_HIGH:
-        print(f"Your guess is out of bounds.")
-        print(f"It must be between {RANGE_LOW} and {RANGE_HIGH}")
-    elif user_input == random_number:
-        print("You guessed the number!  Good job!")
-    elif user_input > random_number:
-        print("Your guess is too high")
-    elif user_input < random_number:
-        print("Your guess is too low")
+    random_number = random.randint(RANGE_LOW, RANGE_HIGH)
+
+    waiting_for_correct_guess = True
+    while waiting_for_correct_guess:
+        user_input = get_number_from_user()
+        
+        if user_input < RANGE_LOW or user_input > RANGE_HIGH:
+            print(f"Your guess is out of bounds.")
+            print(f"It must be between {RANGE_LOW} and {RANGE_HIGH}")
+        elif user_input == random_number:
+            print("You guessed the number!  Good job!")
+        elif user_input > random_number:
+            print("Your guess is too high")
+        elif user_input < random_number:
+            print("Your guess is too low")
     
 
 def get_number_from_user():
-    user_input_string = input("Guess the number: ")
+    valid_input = False
     user_input = None
-    if user_input_string.isnumeric():
-        user_input = int(user_input_string)
-    else:
-        print("You must input a number!")
-        
+    while not valid_input:
+        user_input_string = input("Guess the number: ")
+
+        if user_input_string.isnumeric():
+            user_input = int(user_input_string)
+            valid_input = True
+        else:
+            print("You must input a number!")
+
     return user_input
 
 guess_the_number()
