a5bba09af83451983ca16364327d5a027c9af222
Vida Maleki <vida.basmani@gmail.com>
2022-03-17 11:41:50 -0700

    first commit

--- unit1/main.py
+++ unit1/main.py
@@ -3,7 +3,7 @@ VALID_CARDS = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace']
 def blackjack_score(hand):
     score = 0
     num_aces = 0
-  
+
     for card in hand:
         if card not in VALID_CARDS or (len(hand) < 2) or (len(hand) > 5):
             return "Invalid"
