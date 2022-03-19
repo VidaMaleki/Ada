95f8e35bc6850ada2630df096ae43a0008c34341
Vida Maleki <vida.basmani@gmail.com>
2022-03-19 09:39:39 -0700

    file is only for practice and debuging

---
+++ unit1/Example.py
@@ -0,0 +1,28 @@
+janes_data = {'watched': [], 'watchlist': [{'genre': 'Horror', 'rating': 3.5, 'title': 'It Came from the Stack Trace'}]}
+MOVIE_TITLE_1 = 'It Came from the Stack Trace'
+
+
+
+
+# def watch_movie(user_data, title):
+#     watchlist = user_data['watchlist']
+#     new_watchlist = []
+#     print(watchlist)
+#     watched = user_data['watched']
+#     for item in range(len(list(watchlist))):
+#         if watchlist[item]["title"] != title:
+            
+#             new_watchlist.append(watchlist[item])
+#         else:
+#             watched.append[title]
+#     print(watched)
+#     user_data["watchlist"] = new_watchlist
+#     return user_data
+# def watch_movie(janes_data, MOVIE_TITLE_1):
+#     new_list = []
+#     if janes_data.get("watchlist")[0].get("title") == MOVIE_TITLE_1:
+#         new_list.append(janes_data.get("watchlist")[0].pop("title"))
+#     return new_list
+# print(watch_movie(janes_data, MOVIE_TITLE_1))
+
+print(janes_data)
